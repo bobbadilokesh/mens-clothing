ab29e6aafb4a0163b59e69f909f0312c7e9ed4f1
bobbadilokesh <bobbadhiloki@gmail.com>
2026-02-26 10:51:30 +0530

    Fixed CI build issue - removed unused imports

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ import HomePage from "./components/HomePage";
 import ScrollToTop from "./components/ScrollToTop";
 
 import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Category from './components/Category';
 import Shirts from './components/Shirts';
-import Newarrivals from './components/Newarrivals';
-import Footer from './components/Footer';
 import Tshirts from './components/Tshirts';
 import Bottoms from './components/Bottoms';
 import Cart from './components/Cart';
@@ -19,9 +15,9 @@ import Jackets from './components/jackets';
 import Sweatshirts from './components/Sweatshirts';
 import Trouser from './components/Trouser';
 import AllProducts from './components/AllProducts';
-import Service from './components/Service';
+import Footer from './components/Footer';
 import Faq from './components/Faq';
-import Shipping from './components/Shipping'; 
+import Shipping from './components/Shipping';
 import ReturnPolicy from './components/ReturnPolicy';
 import PrivacyPolicy from './components/PrivacyPolicy';
 import ContactUs from './components/ContactUs';
@@ -35,17 +31,6 @@ function App() {
           <Navbar />
 
           <Routes>
-            {/* <Route
-              path="/"
-              element={
-                <>
-                  <Home />
-                  <Category />
-                  <Newarrivals />
-                  <Service />
-                </>
-              }
-            /> */}
             <Route path="/" element={<HomePage />} />
             <Route path="/shirts" element={<Shirts />} />
             <Route path="/tshirts" element={<Tshirts />} />
